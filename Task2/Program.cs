65818058d9d99134766c4aea23eb65c896ee37b8
mila <milavoronina1997@mail.ru>
2022-09-04 17:43:47 +0500

    Task2_1

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//452 -> 11
+
+//82 -> 10
+
+//9012 -> 12
+int NumberSum(int a)
+{
+    string b = Convert.ToString(a);
+    int length = b.Length;
+    int sum = 0;
+    int res = 0;
+    for (int i = 0; i < length; i++)
+    {
+        res = a - (a % 10);
+        sum = sum + (a - res);
+        a = (a / 10);
+    }
+    return sum;
+
+}
+try
+{
+    Console.WriteLine("Введите любое целое число: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    int sum = NumberSum(a);
+    Console.WriteLine(sum);
+}
+catch
+{
+    Console.WriteLine("Please enter integer number");
+}
