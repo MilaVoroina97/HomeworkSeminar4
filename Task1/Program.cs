ce646f3fc0e1a6c89fe6b0a0669d6479f2f9736c
mila <milavoronina1997@mail.ru>
2022-09-04 16:50:00 +0500

    Task1_1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+//3, 5 -> 243 (3⁵)
+
+//2, 4 -> 16
+
+// First way:
+
+
+// void Stepen(int x, int y)
+
+// {  
+//     Console.WriteLine($"{x}^{y} = {Math.Pow(x, y)}");
+// }
+
+// Console.WriteLine("Enter first integer number");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter second integer number");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Stepen(a,b);
+
+// Second Way:
+
+
+double Stepen(int x,int y)
+{
+    double power = Math.Pow(x,y);
+    Console.WriteLine($"{x}**{y} = {power}");
+    return power;
+}
+Console.WriteLine("Enter first integer number");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second integer number");
+int b = Convert.ToInt32(Console.ReadLine());
+double res = Stepen(a,b);
+
+
+
+
