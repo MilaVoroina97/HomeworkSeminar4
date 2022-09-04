ca54774f471bc923630a4c6b65ee83631fbd71fe
mila <milavoronina1997@mail.ru>
2022-09-04 23:53:03 +0500

    Task4_1

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу сортировки массива от большего к меньшему. Массив задается размерностью 
+//N с клавиатуры, далее заполняется случайными целыми числами.
+
+void FillArray (int[] massiv)
+{
+    for (int i = 0; i < massiv.Length; i++)
+    {
+        massiv[i] = new Random().Next(1, 100);
+    }
+}
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SortArray(int[] array)
+{
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        int max_position = i;
+        for(int j = i+1; j < array.Length; j++)
+        {
+            if(array[j] > array[max_position]) 
+            {
+                max_position = j;
+            }
+        }
+        int temp = array[i];
+        array[i] = array[max_position];
+        array[max_position] = temp;
+    }
+}
+Console.WriteLine("Введите число элементов массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] result = new int[N];
+FillArray(result);
+PrintArray(result);
+SortArray(result);
+PrintArray(result);
