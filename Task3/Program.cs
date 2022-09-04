3de9a00ee50d443199a8c0a44fe85ee963e165c6
mila <milavoronina1997@mail.ru>
2022-09-04 23:12:21 +0500

    Task3_2

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+//6, 1, 33 -> [6, 1, 33]
+
+
+int[] FillArray(string x)
+{
+    int[] array = new int[8];
+    int j = 0;
+    for (int i = 0; i < x.Length; i++)
+    {
+        string y = "";
+        y = y + x[i];
+        array[j] = Convert.ToInt32(y);
+        j++;
+    }
+    return array;
+}
+    void PrintArray(int[] massiv)
+    {
+        int size = massiv.Length;
+        Console.Write("[");
+        for(int i = 0; i < size; i++)
+        {
+            Console.Write(massiv[i]);
+            Console.Write(",");
+        }
+        
+         Console.Write("]");
+    }
+try
+{
+    Console.WriteLine("Введите 8 целых чисел");
+    string N = Console.ReadLine();
+    int[] result = FillArray(N);
+    PrintArray(result);
+}
+catch
+{
+    Console.WriteLine("Please enter integer numbers without spaces and dots");
+}
+
+    
+
+
